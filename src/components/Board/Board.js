3a331139b04e4283c8e23b0513c3cd3856819202
zl95826 <zl95826@hotmail.com>
2019-10-01 12:21:38 -0700

    use a method to render squares in Board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,23 +2,41 @@ import React,{Fragment} from 'react';
 import style from './Board.module.css';
 import Square from '../Square/Square';
 
-const Board=(props)=><div>
-                        <section className={style.Row}>
-                            <Square value={props.squares[0]} index={0} click={props.click}/>
-                            <Square value={props.squares[1]} index={1} click={props.click}/>
-                            <Square value={props.squares[2]} index={2} click={props.click}/>
-                        </section>
-                        <section className={style.Row}>
-                            <Square value={props.squares[3]} index={3} click={props.click}/>
-                            <Square value={props.squares[4]} index={4} click={props.click}/>
-                            <Square value={props.squares[5]} index={5} click={props.click}/>
-                        </section>
-                        <section className={style.Row}>
-                            <Square value={props.squares[6]} index={6} click={props.click}/>
-                            <Square value={props.squares[7]} index={7} click={props.click}/>
-                            <Square value={props.squares[8]} index={8} click={props.click}/>
-                        </section>
-                    </div>
+const Board=(props)=>{
+        const renderSquare=(i)=><Square value={props.squares[i]} index={i} click={props.click} />
+        return  <div>
+                    <section className={style.Row}>
+                        {renderSquare(0)}
+                        {renderSquare(1)}
+                        {renderSquare(2)}
+                    {/*
+                        <Square value={props.squares[0]} index={0} click={props.click}/>
+                        <Square value={props.squares[1]} index={1} click={props.click}/>
+                        <Square value={props.squares[2]} index={2} click={props.click}/>
+                    */}
+                    </section>
+                    <section className={style.Row}>
+                        {renderSquare(3)}
+                        {renderSquare(4)}
+                        {renderSquare(5)}
+                    {/*
+                        <Square value={props.squares[3]} index={3} click={props.click}/>
+                        <Square value={props.squares[4]} index={4} click={props.click}/>
+                        <Square value={props.squares[5]} index={5} click={props.click}/>
+                    */}
+                    </section>
+                    <section className={style.Row}>
+                        {renderSquare(6)}
+                        {renderSquare(7)}
+                        {renderSquare(8)}
+                    {/*
+                        <Square value={props.squares[6]} index={6} click={props.click}/>
+                        <Square value={props.squares[7]} index={7} click={props.click}/>
+                        <Square value={props.squares[8]} index={8} click={props.click}/>
+                    */}
+                    </section>
+                </div>
+}
 
 
 export default Board;
