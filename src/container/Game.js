6f82b61f1631bbe31243e8b74f812776a45e96d3
zl95826 <zl95826@hotmail.com>
2019-10-01 11:58:54 -0700

    add replay function

--- src/container/Game.js
+++ src/container/Game.js
@@ -13,11 +13,15 @@ class Game extends Component {
         newArr[e.target.id]='X';
         this.setState({squares:newArr}); 
     }
+    clickReplay=()=>{
+        const newArr=Array(9).fill(null);
+        this.setState({squares:newArr}); 
+    }
     render() {
         return <div className={styles.Board}>
                     <h1>Tic Tac Toe</h1>
                     <Board squares={this.state.squares} click={this.clickSquare}/>
-                    <button className={styles.Replay}>Replay</button>
+                    <button className={styles.Replay} onClick={this.clickReplay}>Replay</button>
                 </div>;
     }
 
