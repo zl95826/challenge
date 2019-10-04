cfc9b6072e060382bd7957cc8d7b0c42aa06df93
zl95826 <zl95826@hotmail.com>
2019-10-03 23:13:32 -0700

    Updates

---
+++ precache-manifest.babb30bf386dcbf130bbd48c4cd430ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca1720c46fa41bf469bcc3321d2de1a",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "267b3110c440bddd8669",
+    "url": "/challenge/static/css/main.9a8cb5c7.chunk.css"
+  },
+  {
+    "revision": "a0e2a21aae221619e631",
+    "url": "/challenge/static/js/2.46f912d3.chunk.js"
+  },
+  {
+    "revision": "267b3110c440bddd8669",
+    "url": "/challenge/static/js/main.7839fb8d.chunk.js"
+  },
+  {
+    "revision": "0d280a0d4a797a0ec4ee",
+    "url": "/challenge/static/js/runtime-main.1c4276e7.js"
+  }
+]);
