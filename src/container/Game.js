021eda3e01d7d1c1af204329cc3b7dcad7547944
zl95826 <zl95826@hotmail.com>
2019-12-10 17:52:16 -0800

    test

--- src/container/Game.js
+++ src/container/Game.js
@@ -18,7 +18,7 @@ class Game extends Component {
     }
     componentDidMount() {
         console.log('test');
-        axios.get('https://api.stocktwits.com/api/2/streams/user/2956810.json')
+        axios.get('/user/2956810.json')
         .then(res=>console.log(res.data.user));
     }
     clickSquare=(e)=>{
